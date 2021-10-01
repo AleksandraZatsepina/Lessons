065902c57fb03ee06d3c71c3e87bb8612348ddee
AleksandraZatsepina <90957396+AleksandraZatsepina@users.noreply.github.com>
2021-10-01 15:14:55 +0300

    Update Task1.4.cpp

--- Task1.4.cpp
+++ Task1.4.cpp
@@ -10,13 +10,13 @@ int main()
     cin >> a;
     cout << "Введите b = ";
     cin >> b;
-    float S = a + b;
-    float R = a - b;
-    float P = a * b;
+    float S = (a * a) + (b * b);
+    float R = (a * a) - (b * b);
+    float P = (a * a) * (b * b);
     float K = (a * a)  / (b * b);
-    cout << "a + b = " << S << endl;
-	cout << "a - b = " << R << endl;
-	cout << "a * b = " << P << endl;
+    cout << "(a * a) + (b * b) = " << S << endl;
+	cout << "(a * a) - (b * b) = " << R << endl;
+	cout << "(a * a) * (b * b) = " << P << endl;
 	cout << "(a * a)  / (b * b) = " << K << endl;
     return 0;
 }
